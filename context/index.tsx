4008eabaacecffb1823909b8b47c425716917b1a
pheuberger <philipp@pheuberger.com>
2024-11-07 09:15:10 +0200

    Revert "feat: implement silk wallet"
    This reverts commit ea94367cfeb210fbdbf42ae0e084b359b9ae58b2.

--- context/index.tsx
+++ context/index.tsx
@@ -1,15 +1,23 @@
 "use client";
-import "@rainbow-me/rainbowkit/styles.css";
 import React from "react";
 
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider } from "wagmi";
+import { WagmiProvider, http } from "wagmi";
+import { sepolia } from "wagmi/chains";
 
 import "@/app/globals.css";
 
-import { wagmiConfig } from "@/wagmi/config";
-
+export const wagmiConfig = getDefaultConfig({
+  appName: process.env.NEXT_PUBLIC_APP_NAME!,
+  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID!,
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http(`https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY!}`),
+  },
+  ssr: true,
+});
 const client = new QueryClient();
 
 export default function ContextProvider({ children }: { children: React.ReactNode }) {
